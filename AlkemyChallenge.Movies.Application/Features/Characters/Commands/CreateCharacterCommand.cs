45b0b290ce32e442a1edc6998909bb7e58218024
Olivera Leonardo <olezdev@gmail.com>
2024-09-26 17:39:27 -0300

    feat: add character CRUD operations and NotFoundException

---
+++ AlkemyChallenge.Movies.Application/Features/Characters/Commands/CreateCharacterCommand.cs
@@ -0,0 +1,70 @@
+﻿using AlkemyChallenge.Movies.Application.Contracts.Persistence;
+using AlkemyChallenge.Movies.Domain.Entities;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+
+namespace AlkemyChallenge.Movies.Application.Features.Characters.Commands;
+
+public class CreateCharacterCommand : IRequest<int>
+{
+    public string Image { get; set; }
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    public decimal Weight { get; set; }
+    public string? History { get; set; }
+    public override string ToString()
+    {
+        return $"Character name: {Name}; Age: {Age}; Weight: {Weight}; Hisotry: {History}";
+    }
+}
+
+public class CreateCharacterCommandHandler : IRequestHandler<CreateCharacterCommand, int>
+{
+    private readonly ICharacterRepository _characterRepository;
+    private readonly IMapper _mapper;
+
+    public CreateCharacterCommandHandler(ICharacterRepository characterRepository, IMapper mapper)
+    {
+        _characterRepository = characterRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<int> Handle(CreateCharacterCommand request, CancellationToken cancellationToken)
+    {
+        var character = _mapper.Map<Character>(request);
+        await _characterRepository.AddAsync(character);
+        await _characterRepository.SaveChangesAsync();
+        return character.Id;
+    }
+}
+
+public class CreateCharacterCommandValidator : AbstractValidator<CreateCharacterCommand>
+{
+    public CreateCharacterCommandValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .NotNull()
+            .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+        RuleFor(p => p.Age)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .NotNull()
+            .GreaterThan(0);
+        RuleFor(p => p.Weight)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .NotNull()
+            .GreaterThan(0);
+        RuleFor(p => p.History)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .NotNull();
+    }
+}
+
+public class CreateCharacterCommandProfile : Profile
+{
+    public CreateCharacterCommandProfile()
+    {
+        CreateMap<CreateCharacterCommand, Character>();
+    }
+}
---
+++ AlkemyChallenge.Movies.Application/Features/Characters/Commands/DeleteCharacterCommand.cs
@@ -0,0 +1,34 @@
+﻿using AlkemyChallenge.Movies.Application.Contracts.Persistence;
+using AlkemyChallenge.Movies.Application.Exceptions;
+using AlkemyChallenge.Movies.Domain.Entities;
+using MediatR;
+
+namespace AlkemyChallenge.Movies.Application.Features.Characters.Commands;
+
+public class DeleteCharacterCommand : IRequest<Unit>
+{
+    public int Id { get; set; }
+}
+
+public class DeleteCharacterCommandHandler : IRequestHandler<DeleteCharacterCommand, Unit>
+{
+    private readonly ICharacterRepository _characterRepository;
+
+    public DeleteCharacterCommandHandler(ICharacterRepository characterRepository)
+    {
+        _characterRepository = characterRepository;
+    }
+
+    public async Task<Unit> Handle(DeleteCharacterCommand request, CancellationToken cancellationToken)
+    {
+        var character = await _characterRepository.GetByIdAsync(request.Id);
+        if (character == null)
+        {
+            throw new NotFoundException(nameof(Character), request.Id);
+        }
+
+        await _characterRepository.DeleteAsync(character);
+        await _characterRepository.SaveChangesAsync();
+        return Unit.Value;
+    }
+}
